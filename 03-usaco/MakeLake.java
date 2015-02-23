4db5e8740e4799fd5cafdc855682db63d263d2cb
tli123 <tommyli12345@gmail.com>
2015-02-23 09:25:34 -0500

    Working

--- 03-usaco/MakeLake.java
+++ 03-usaco/MakeLake.java
@@ -3,11 +3,17 @@ import java.io.*;
 
 public class MakeLake {
 
-    public int[][] lake;
+    public int[][] lake = new int[4][6];
 
     public MakeLake() {
 	try {
-	    Scanner sc = new Scanner(new File
+	    Scanner sc = new Scanner(new File("makelake.txt"));
+	    int x = 0;
+	    while (sc.hasNext()) {
+		String s  = sc.nextLine();
+		x++;
+	    }
+	} catch (Exception e){}
     }
 
 }
---
+++ 03-usaco/MakeLake.java~
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class MakeLake {
+
+    public int[][] lake;
+
+    public MakeLake() {
+	try {
+	    Scanner sc = new Scanner(new File
+    }
+
+}
