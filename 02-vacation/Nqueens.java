fee5681a082e6bf576db7026e2decd721f5f1d94
Tommy Li <tommyli12345@gmail.com>
2015-02-25 13:22:25 -0500

    Sorted!

--- 02-vacation/Nqueens.java
+++ 02-vacation/Nqueens.java
@@ -1,3 +1,63 @@
-public class Nqueens {
+public class KnightsTour {
+
+
+    public int[][] board;
+    public boolean solved = false;
+    public int move = 1;
+
+    public KnightsTour(int row, int col) {
+	board = new int[row][col];
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+	        board[i][j] = 0;
+	    }
+	}
+    }
+
+    public String toString() {
+	String grid = "";
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		grid += board[i][j] + " ";
+	    }
+	    grid += "\n";
+	}
+	return grid;
+    }
+
+    public void HoldUp(int n) {
+	try {
+	    Thread.sleep(n);
+	} catch (Exception e) {}
+    }
+
+    public boolean checker() {
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+	        if (board[i][j] <= 0) {
+		    return false;
+		}
+	    }
+	}
+	return true;
+    }
+
+    public boolean solve(int x, int y) {
+	solve(x+1, y+2);
+	solve(x+1, y-2);
+	solve(x-1, y+2);
+	solve(x-1, y-2);
+	solve(x+2, y+1);
+	solve(x+2, y-1);
+	solve(x-2, y+1);
+	solve(x-2, y-1);
+    }
+
+    public static void main(String[] args) {
+	KnightsTour tour = new KnightsTour(10, 10);
+	System.out.println(tour.toString());
+	tour.solve(1,0);
+	System.out.println("The solution to this Knights Tour is\n" + tour.toString());
+    }
 
 }
--- 04-mergesort/Mergesort.java
+++ 04-mergesort/Mergesort.java
@@ -3,13 +3,45 @@ import java.io.*;
 
 public class Mergesort {
 
-    public ArrayList<Integer> Merge(ArrayList<Integer> a, Arraylist<Integer> b) {
+    public ArrayList<Integer> Merge(ArrayList<Integer> a, ArrayList<Integer> b) {
 	ArrayList<Integer> ans = new ArrayList<Integer>();
 	int acount = 0;
 	int bcount = 0;
 	while (acount < a.size() && bcount < b.size()) {
-	    
+	    if (a.get(acount) < b.get(bcount)) {
+		ans.add(a.get(acount));
+		acount++;
+	    }
+	    else {
+		ans.add(b.get(bcount));
+		bcount++;
+	    }
 	}
+	if (acount < a.size()) {
+	    for (int i = acount; i < a.size(); i++) {
+		ans.add(a.get(i));
+	    }
+	}
+	else {
+	    for (int i = bcount; i < b.size(); i++) {
+		ans.add(b.get(i));
+	    }
+	}
+	return ans;
     }
-
+    public static void main(String[] args) {
+	Mergesort s = new Mergesort();
+	ArrayList<Integer> a = new ArrayList<Integer>();
+	ArrayList<Integer> b = new ArrayList<Integer>();
+	a.add(2);
+	a.add(4);
+	a.add(7);
+	a.add(10);
+	b.add(3);
+	b.add(6);
+	b.add(6);
+	b.add(7);
+	b.add(17);
+	System.out.println(s.Merge(a, b));
+    }
 }
