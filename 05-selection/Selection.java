1a667bda863a9afc62678c2a71cc880d0a6384f0
tli123 <tommyli12345@gmail.com>
2015-03-05 09:23:16 -0500

    Selecting

--- 05-selection/Selection.java
+++ 05-selection/Selection.java
@@ -3,4 +3,37 @@ import java.util.*;
 
 public class Selection {
 
+    /*
+    public static int smallest(int k, int[] nums) {
+	System.out.println("Unsorted Array: " + Arrays.toString(nums));
+	Arrays.sort(nums);
+	System.out.println("Sorted Array: " + Arrays.toString(nums));
+	return nums[k];
+    }
+    
+    public static void main(String[] args) {
+	Selection s = new Selection();
+	int[] a = new int[100];
+	for (int i = 0; i < 100; i++) {
+	    Random r = new Random();
+	    a[i] = r.nextInt(100);
+	}
+	Random w = new Random();
+	int k = w.nextInt(100);
+	String statement = "";
+	if (k%10 == 1) {
+	    statement = "The " + k + "st smallest element: ";
+	}
+	if (k%10 == 2) {
+	    statement = "The " + k + "nd smallest element: ";
+	}
+	if (k%10 == 3) {
+	    statement = "The " + k + "rd smallest element: ";
+	}
+	else if (k%10 == 0 || k%10 > 3) {
+	    statement = "The " + k + "th smallest element: ";
+	}
+	System.out.println(statement + smallest(k, a));
+    }
+    */
 }
