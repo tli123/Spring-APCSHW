66aebedbe2844783462fb788f1e284153bca5d29
tli123 <tommyli12345@gmail.com>
2015-03-20 09:26:16 -0400

    Working

---
+++ 09-iterator/LLit.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class LLit<E> implements Iterator<E>{
+    private Node<E> t;
+    public LLit(Node<E> n){
+	t=n;
+    }
+
+    public boolean hasNext(){
+	return t!=null;
+    }
+
+    public E next(){
+	E retval = t.getData();
+	t=t.getNext();
+	return retval;
+    }
+
+    public void remove(){
+	if (hasNext() == false) { 
+	    E next = next();
+	}
+	
+    }
+}
+
