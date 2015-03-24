32e4f63f8032426cfd0792c4972af2369781e9c9
tli123 <tommyli12345@gmail.com>
2015-03-24 09:26:34 -0400

    Finishing

--- 10-stack/LList.java
+++ 10-stack/LList.java
@@ -2,32 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class LList<E> implements Iterable<E>{
-		private Node<E> l;
+    private Node<E> l;
 		
-		public LList(){
-				l = new Node<E>();
-		}
+    public LList(){
+	l = new Node<E>();
+    }
 
-		public Iterator<E> iterator(){
-				LLit<E> i = new LLit<E>(l.getNext());
-				return i;
-		}
+    public Iterator<E> iterator(){
+	LLit<E> i = new LLit<E>(l.getNext());
+	return i;
+    }
 		
 
 		
-		public void add(E s){
-				Node<E> tmp = new Node<E>(s);
-				tmp.setNext(l.getNext());
-				l.setNext(tmp);
-		}
+    public void add(E s){
+	Node<E> tmp = new Node<E>(s);
+	tmp.setNext(l.getNext());
+	l.setNext(tmp);
+    }
 		
-		public String toString(){
-				String s = "";
-				Node<E> tmp;;
-				for (tmp=l.getNext() ; tmp!=null ; tmp=tmp.getNext()){
-						s = s + tmp + " --> ";
-				}
-				s = s + "null";
-				return s;
-		}
+    public String toString(){
+	String s = "";
+	Node<E> tmp;;
+	for (tmp=l.getNext() ; tmp!=null ; tmp=tmp.getNext()){
+	    s = s + tmp + " --> ";
+	}
+	s = s + "null";
+	return s;
+    }
 }
--- 10-stack/myStack.java
+++ 10-stack/myStack.java
@@ -1,22 +1,49 @@
 public class myStack<E> {
 
-    public void push(E data) {
+    private Node<E> dummy = new Node<E>();
+    private Node<E> top;
 
+    public myStack() {
+	top = new Node<E>();
     }
 
-    public E pop() {
+    public void push(E data) {
+	Node<E> tmp = dummy;
+	while (tmp != null) {
+	    tmp = tmp.getNext();
+	}
+	Node<E> n = new Node<E>(data);
+	n.setNext(tmp.getNext());
+	tmp.setNext(n);
+    }
 
+    /*
+    public E pop() {
+	
     }
 
     public boolean empty() {
-
+	
     }
 
-    public E top() {
+    */
 
+    public E top() {
+	return top.getData();
+    }
+
+    public String toString(){
+	String s = "";
+	Node tmp;
+	for (tmp = dummy ; tmp != null ; tmp = tmp.getNext()){
+	    s = s + tmp + " --> ";
+	}
+	s = s + "null";
+	return s;
     }
 
     public static void main(String[] args) {
 
     }
+
 }
