bb8bcf83220535c057c6f799aaeafaf65c56af97
tli123 <tommyli12345@gmail.com>
2015-04-15 09:11:58 -0400

    Working

---
+++ 13-A*/Frontier.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Frontier {
+    public LinkedList<Node> l = new LinkedList<Node>();
+
+    public void add(Node n){
+	l.add(n);
+    }
+
+    public Node remove() {
+	return l.remove(0);
+    }
+
+    public boolean isEmpty(){
+	return l.isEmpty();
+    }
+}
+
+
+
+
+
+
+
+
+
+
